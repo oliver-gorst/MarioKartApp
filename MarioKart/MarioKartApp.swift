2f95191ea82ac6f8e269cf456966c0f2fc23d8d5
Oliver Gorst <oliver.gorst@outlook.com>
2022-01-21 14:29:30 -0500

    Initial Commit

---
+++ MarioKart/MarioKartApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarioKartApp.swift
+//  MarioKart
+//
+//  Created by Oliver Gorst on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MarioKartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
