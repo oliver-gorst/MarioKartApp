6ff70c1dd12e90b6f80994b486fee13d46d700c3
Oliver Gorst <oliver.gorst@outlook.com>
2022-02-02 23:23:05 -0500

    Added track names on outside layer. Need to add delete functionality to the initial titles

--- MarioKart/CupList.swift
+++ MarioKart/CupList.swift
@@ -13,6 +13,8 @@ struct CupList: View {
     
     private var cupImageName = ["Mushroom", "Shell", "Flower", "Banana", "Star", "Leaf", "Special", "Lightning", "Egg", "Illuminati", "Crossing", "Bell"]
     
+    private var allTrackNames = ["Mario Kart Stadium", "Water Park", "Sweet Sweet Canyon", "Thwomp Ruins", "Moo Moo Meadows", "Mario Circuit", "Cheep Cheep Beach", "Toad's Turnpike", "Mario Circuit", "Toad Harbor", "Twisted Mansion", "Shy Guy Falls", "Dry Dry Desert", "Donut Plains 3", "Royal Raceway", "DK Jungle", "Sunshine Airport", "Dolphin Shoals", "Electrodome", "Mount Wario", "Wario Stadium", "Sherbet Land", "Music Park", "Yoshi Valley", "Cloudtop Cruise", "Bone Dry Dunes", "Bowser's Castle", "Rainbow Road", "Tick Tock Clock", "Piranha Plant Slide", "Grumble Volcano", "Rainbow Road", "Yoshi's Circuit", "Excitebike Arena", "Dragon Driftway", "Mute City", "Wario's Goldmine", "Rainbow Road", "Ice Ice Outpost", "Hyrule Circuit", "Baby Park", "Cheese Land", "Wild Woods", "Animal Crossing", "Neo Bowser City", "Ribbon Road", "Super Bell Subway", "Big Blue"]
+    
 
     //Need to define the variables in the child views here in the parent view
 
@@ -27,7 +29,17 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[0])
                             .cupImageModifier()
-                        Text(cupNames[0])
+                            VStack{
+                                Text(cupNames[0])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[0])
+                                Text(allTrackNames[1])
+                                Text(allTrackNames[2])
+                                Text(allTrackNames[3])
+                                
+                            }
+                        
                         }
                     }.buttonStyle(PlainButtonStyle())
                     
@@ -35,7 +47,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[1])
                             .cupImageModifier()
-                        Text(cupNames[1])
+                            VStack{
+                            Text(cupNames[1])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[4])
+                                Text(allTrackNames[5])
+                                Text(allTrackNames[6])
+                                Text(allTrackNames[7])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                     
@@ -43,7 +63,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[2])
                             .cupImageModifier()
-                        Text(cupNames[2])
+                            VStack{
+                            Text(cupNames[2])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[8])
+                                Text(allTrackNames[9])
+                                Text(allTrackNames[10])
+                                Text(allTrackNames[11])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                     
@@ -51,7 +79,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[3])
                             .cupImageModifier()
-                        Text(cupNames[3])
+                            VStack{
+                            Text(cupNames[3])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[12])
+                                Text(allTrackNames[13])
+                                Text(allTrackNames[14])
+                                Text(allTrackNames[15])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                     
@@ -59,7 +95,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[4])
                             .cupImageModifier()
-                        Text(cupNames[4])
+                            VStack{
+                            Text(cupNames[4])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[16])
+                                Text(allTrackNames[17])
+                                Text(allTrackNames[18])
+                                Text(allTrackNames[19])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                     
@@ -67,7 +111,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[5])
                             .cupImageModifier()
-                        Text(cupNames[5])
+                            VStack{
+                            Text(cupNames[5])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[20])
+                                Text(allTrackNames[21])
+                                Text(allTrackNames[22])
+                                Text(allTrackNames[23])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                 }
@@ -76,7 +128,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[6])
                             .cupImageModifier()
-                        Text(cupNames[6])
+                            VStack{
+                            Text(cupNames[6])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[24])
+                                Text(allTrackNames[25])
+                                Text(allTrackNames[26])
+                                Text(allTrackNames[27])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                     
@@ -84,7 +144,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[7])
                             .cupImageModifier()
-                        Text(cupNames[7])
+                            VStack{
+                            Text(cupNames[7])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[28])
+                                Text(allTrackNames[29])
+                                Text(allTrackNames[30])
+                                Text(allTrackNames[31])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                     
@@ -92,7 +160,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[8])
                             .cupImageModifier()
-                        Text(cupNames[8])
+                            VStack{
+                            Text(cupNames[8])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[32])
+                                Text(allTrackNames[33])
+                                Text(allTrackNames[34])
+                                Text(allTrackNames[35])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                     
@@ -100,7 +176,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[9])
                             .cupImageModifier()
-                        Text(cupNames[9])
+                            VStack{
+                            Text(cupNames[9])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[36])
+                                Text(allTrackNames[37])
+                                Text(allTrackNames[38])
+                                Text(allTrackNames[39])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                     
@@ -108,7 +192,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[10])
                             .cupImageModifier()
-                        Text(cupNames[10])
+                            VStack{
+                            Text(cupNames[10])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[40])
+                                Text(allTrackNames[41])
+                                Text(allTrackNames[42])
+                                Text(allTrackNames[43])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                     
@@ -116,7 +208,15 @@ struct CupList: View {
                         HStack{
                         Image(cupImageName[11])
                             .cupImageModifier()
-                        Text(cupNames[11])
+                            VStack{
+                            Text(cupNames[11])
+                                    .font(.system(size: 20, weight: .heavy, design: .default))
+                                    .underline()
+                                Text(allTrackNames[44])
+                                Text(allTrackNames[45])
+                                Text(allTrackNames[46])
+                                Text(allTrackNames[47])
+                            }
                         }
                     }.buttonStyle(PlainButtonStyle())
                 }
