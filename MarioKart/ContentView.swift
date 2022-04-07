4f600154eb748ce47bffaee51112ca698b95a063
Oliver Gorst <oliver.gorst@outlook.com>
2022-04-06 22:16:20 -0400

    Small change

--- MarioKart/ContentView.swift
+++ MarioKart/ContentView.swift
@@ -43,7 +43,7 @@ struct ContentView: View {
             }) {
                 HStack{
                     Image(systemName:"trash")
-                    Text("Clear Track History")
+                    Text("Clear Track History.")
                 }
                 
             }
